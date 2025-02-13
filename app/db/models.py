e73b2ad973183dff91ea581d3563d58baca80756
Ibragimov <ib_maga_06@mail.ru>
2025-02-13 19:33:18 +0300

    Обновленны модели. добавленно: связь между таблицами, creator_id, status, date

--- app/db/models.py
+++ app/db/models.py
@@ -1,22 +1,44 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import BigInteger
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import BigInteger, Text, func, DateTime, ForeignKey, String
+from datetime import datetime
 
 from app.db.database import Base
 
 
-class Tasks(Base):
-    __tablename__ = 'tasks'
-
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
-    name: Mapped[str] = mapped_column(nullable=False)
-    description: Mapped[str] = mapped_column(nullable=True)
-
-
 class Users(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
-    username: Mapped[str] = mapped_column(nullable=False, unique=True)
-    password: Mapped[str] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, autoincrement=True, nullable=False
+    )
+    username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    role: Mapped[str] = mapped_column(String, nullable=False, default="quest")
 
-    role: Mapped[str] = mapped_column(nullable=False)
+    # Связь: пользователь может создать несколько задач
+    created_tasks: Mapped[list["Tasks"]] = relationship(
+        "Tasks", back_populates="creator", primaryjoin="Users.id==Tasks.creator_id"
+    )
+
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, autoincrement=True, nullable=False
+    )
+    name: Mapped[str] = mapped_column(String, nullable=False)
+
+    # Изменяем поле: вместо ссылки на username используем creator_id, который ссылается на Users.id
+    creator_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="SET NULL"), nullable=True
+    )
+
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    status: Mapped[str] = mapped_column(String, nullable=False, default="created")
+    date: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=func.now(), server_default=func.now()
+    )
+
+    # Отношение к пользователю (создателю)
+    creator: Mapped["Users"] = relationship("Users", back_populates="created_tasks")
