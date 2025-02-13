51bfe8518e7076257f513fed6cca98c933757319
Ibragimov <ib_maga_06@mail.ru>
2025-02-13 19:40:55 +0300

    добавлены функции связанные с токенами

--- app/core/security.py
+++ app/core/security.py
@@ -1,14 +1,16 @@
 import jwt
-from passlib.hash import scrypt
-from datetime import datetime, timedelta
 from fastapi import Depends, Response
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta, timezone
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, HTTPBearer
+from passlib.hash import scrypt
 
-from app.api.schemes.user import UserFromDB, User, Tokens
+from app.api.schemes.other import Payload, Tokens
+from app.api.schemes.user import UserFromDB, User, UserOut
 from app.core.configs import settings
-from app.exc.exception import AuthenticationError
+from app.exc.exception import AuthenticationError, TokenError
 
 oauth_scheme = OAuth2PasswordBearer(tokenUrl="/auth/log-in/")
+http_bearer = HTTPBearer(auto_error=False)
 
 TOKEN_TYPE_FIELD = "type"
 ACCESS_TOKEN_TYPE = "access"
@@ -27,9 +29,15 @@ def get_user_view(user: UserFromDB) -> User:
     return User(username=user.username, password=user.password, role=user.role)
 
 
-def authentication(userdata: OAuth2PasswordRequestForm, user: UserFromDB) -> User:
+async def authentication(
+    userdata: OAuth2PasswordRequestForm, user: UserFromDB, user_crud
+) -> UserOut:
     if verify_pass(userdata.password, user.password):
-        return get_user_view(user)
+        """Изменение роли пользователя при авторизации на user"""
+        if user.role != "user":
+            user.role = "user"
+            await user_crud.update_user(user_id=user.id, user=user)
+        return UserOut.model_validate(user)
     raise AuthenticationError
 
 
@@ -41,7 +49,7 @@ def encode_jwt(
     expire_minutes: int = settings.EXP_ACCESS,
     expire_timedelta: timedelta | None = None,
 ) -> str:
-    now = datetime.now()
+    now = datetime.now(timezone.utc)
     if expire_timedelta:
         exp = now + expire_timedelta
     else:
@@ -53,13 +61,13 @@ def encode_jwt(
     return jwt.encode(payload=jwt_payload, key=private_key, algorithm=algorithm)
 
 
-def create_access_token(user: User) -> str:
-    jwt_payload = {"sub": user.username, "role": user.role}
+def create_access_token(user: UserFromDB) -> str:
+    jwt_payload = {"sub": user.username, "id": user.id, "role": user.role}
     return encode_jwt(token_type=ACCESS_TOKEN_TYPE, token_data=jwt_payload)
 
 
-def create_refresh_token(user: User):
-    jwt_payload = {"sub": user.username}
+def create_refresh_token(user: UserFromDB):
+    jwt_payload = {"sub": user.username, "id": user.id}
     return encode_jwt(
         token_type=REFRESH_TOKEN_TYPE,
         token_data=jwt_payload,
@@ -67,12 +75,30 @@ def create_refresh_token(user: User):
     )
 
 
-def decode_jwt(token: str):
+def decode_jwt(token: str) -> dict:
     return jwt.decode(jwt=token, key=settings.SECRETKEY, algorithms=settings.ALGORITHM)
 
 
-def is_jwt(token: str = Depends(oauth_scheme)):
-    return decode_jwt(token)
+def check_payload_token(token: str, type_token: str):
+    try:
+        payload = decode_jwt(token)
+    except jwt.ExpiredSignatureError:
+        raise TokenError(detail="Token has expired")
+
+    if payload[TOKEN_TYPE_FIELD] != type_token:
+        raise TokenError(
+            detail=f"Invalid token type {payload[TOKEN_TYPE_FIELD]!r} expected {type_token!r}"
+        )
+    return payload
+
+
+class CheckToken:
+    def __init__(self, token_type: str):
+        self.token_type = token_type
+
+    def __call__(self, token: str = Depends(oauth_scheme)):
+        payload = check_payload_token(token, self.token_type)
+        return Payload(**payload)
 
 
 def set_token_to_cookie(response: Response, token: Tokens):
