4cbb244f18874bb6af8809c329c6299931005be3
Ibragimov <ib_maga_06@mail.ru>
2025-02-18 20:25:11 +0300

    update configurations and dependencies for testing support

--- app/core/configs.py
+++ app/core/configs.py
@@ -15,8 +15,17 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
 
+    TEST_DB_HOST: str
+    TEST_DB_NAME: str
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+
+    MODE: str
+
     @property
     def db_url(self):
+        if self.MODE == "TEST":
+            return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}/{self.TEST_DB_NAME}"
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}/{self.DB_NAME}"
 
 
