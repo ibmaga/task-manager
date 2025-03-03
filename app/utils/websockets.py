99a3ea86eef0fe3ec45754f6e55a0c60c225b2d8
Ibragimov <ib_maga_06@mail.ru>
2025-03-03 14:08:06 +0300

    enhance websocket broadcasting to exclude sender's messages

--- app/utils/websockets.py
+++ app/utils/websockets.py
@@ -9,13 +9,16 @@ class ConnectionManager:
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.connections.append(websocket)
+        await self.broadcast("New user", _start=1)
 
     async def disconnect(self, websocket: WebSocket):
         self.connections.remove(websocket)
 
-    async def broadcast(self, message: str):
-        for connection in self.connections:
-            await connection.send_text(message)
+    async def broadcast(self, message: str, _start: int = 0):
+        """_start был добавлен, что бы отправитель не получал свое же сообщение"""
+        for i, connection in enumerate(self.connections, start=_start):
+            if i != len(self.connections):
+                await connection.send_text(message)
 
     @classmethod
     async def send_personal_message(cls, message: str, websocket: WebSocket):
